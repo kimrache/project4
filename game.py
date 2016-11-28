306ee9e7e8d5d3c04f28a2daf41a594fe7b6fdb5
Rachel Kim <kimrache@umich.edu>
2016-11-28 15:00:15 -0500

    new with movement

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-#How to draw shapes
+#Frames per second manipulation
 
 #required 
 import pygame
@@ -11,12 +11,18 @@ red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
 
+#position vars
+x_pos = 0
+y_pos = 0
+x_delta = 0
+y_delta = 0
+clock = pygame.time.Clock()
+
 #create a surface
 gameDisplay = pygame.display.set_mode((800,600)) #initialize with a tuple
 
 #lets add a title, aka "caption"
-pygame.display.set_caption("Drawing Shapes!")
-
+pygame.display.set_caption("Frames per second")
 pygame.display.update()		#only updates portion specified
 
 
@@ -24,27 +30,32 @@ pygame.display.update()		#only updates portion specified
 
 gameExit = False
 while not gameExit:
+	gameDisplay.fill(white)
+
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			gameExit = True
 
-	gameDisplay.fill(white)
-	#look up draw.rect()
-	pygame.draw.rect(gameDisplay, black, [400,300, 10, 100])
-	pygame.draw.rect(gameDisplay, red, [100,100, 50, 50])
-
-	gameDisplay.fill(blue, rect=[200,200, 20,20])
-
-
-	gameDisplay.fill(blue, rect=[50,50, 20,20])
-	pygame.draw.circle(gameDisplay, red, (50,100), 20, 0)
-	pygame.draw.lines(gameDisplay, red, False, [(100,100), (150,200), (200,100)], 1)
-
+	if event.type == pygame.KEYDOWN:
+		x_delta=0;
+		y_delta=0;
+		if event.key == pygame.K_LEFT:
+			x_delta -= 10
+		if event.key == pygame.K_RIGHT:
+			x_delta += 10
+		if event.key == pygame.K_UP:
+			y_delta -= 10
+		if event.key == pygame.K_DOWN:
+			y_delta += 10
+	
+	x_pos +=x_delta
+	y_pos +=y_delta
+	gameDisplay.fill(blue, rect=[x_pos,y_pos, 20,20])
 	pygame.display.update()		
-
+	clock.tick(30)
 
 
 
 #required
 pygame.quit()
-quit()				#exits python
+quit()		
